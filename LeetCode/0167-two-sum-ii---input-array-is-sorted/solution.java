d60b1fa9615b87e1324c52c494cf14ccecabfaaf
Aditya B <aditya.bajoria0208@gmail.com>
2026-03-19 08:35:51 +0000

    [LeetCode Sync] Solved Runtime - 6 ms (12.79%), Memory - 49.2 MB (5.02%)

--- LeetCode/0167-two-sum-ii---input-array-is-sorted/solution.java
+++ LeetCode/0167-two-sum-ii---input-array-is-sorted/solution.java
@@ -1,19 +1,21 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
-        int result[] = new int[2];
-        int low = 0; 
-        int high = numbers.length-1;
-        while(low<high){
-            if(numbers[low] + numbers[high] == target){
-                result[0] = low+1;
-                result[1] = high+1;
+        Map<Integer,Integer> map = new HashMap<>();
+        int result[] = {-1,-1};
+        for(int i=0; i<numbers.length;i++)
+        {
+            int x = target-numbers[i];
+            if(!map.containsKey(x))
+            {
+                map.put(numbers[i],i);
+            }
+            else
+            {
+                int position1 = map.get(x)+1;
+                int position2 = i+1;
+                result[0] = position1;
+                result[1] = position2;
                 return result;
-            }
-            else if(numbers[low]+numbers[high] > target){
-                high--;
-            }
-            else if(numbers[low]+numbers[high] < target){
-                low++;
             }
         }
         return result;
