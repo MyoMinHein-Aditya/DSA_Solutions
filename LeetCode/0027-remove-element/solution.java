b7bb44a7b5f3eb41e976fe2f1806564af7ad1b33
Aditya B <aditya.bajoria0208@gmail.com>
2026-07-21 07:17:06 +0000

    [LeetCode Sync] Solved Runtime - 0 ms (100.00%), Memory - 43.7 MB (9.03%)

--- LeetCode/0027-remove-element/solution.java
+++ LeetCode/0027-remove-element/solution.java
@@ -1,15 +1,15 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
         int []temp = new int[nums.length];
-        int k=0;
-        for(int i=0;i<nums.length;i++){
+        int k = 0;
+        for(int i = 0; i < nums.length;i++){
             if(nums[i]!=val){
-                temp[k]=nums[i];
+                temp[k] = nums[i];
                 k++;
             }
         }
-        for(int i=0;i<k;i++){
-            nums[i]=temp[i];
+        for(int i = 0; i < k; i++){
+            nums[i] = temp[i];
         }
         return k;
     }
