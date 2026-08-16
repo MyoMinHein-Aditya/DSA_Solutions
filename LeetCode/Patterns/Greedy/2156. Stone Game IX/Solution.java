583e5cf1bd066706a7ed48b559b59a5fdd7e6eb3
Aditya B <aditya.bajoria0208@gmail.com>
2026-08-16 19:12:04 +0530

    Add Java Solution to Patterns/Greedy

--- LeetCode/Patterns/Greedy/2156. Stone Game IX/Solution.java
+++ LeetCode/Patterns/Greedy/2156. Stone Game IX/Solution.java
@@ -1,23 +1,21 @@
 class Solution {
 
-    public boolean can_win(int count0, int count1, int count2){
-        if(count1==0) return false;
+    public boolean can_win(int count0, int count1, int count2) {
+        if (count1 == 0) return false;
         count1--;
-        boolean A_turn = false;
-        if(count0 % 2 ==0){
+        if (count0 % 2 == 0) {
             return count2 > count1;
-        }
-        else{
+        } else {
             return count1 > count2 + 2;
         }
     }
 
     public boolean stoneGameIX(int[] stones) {
         int[] countStones = new int[3];
-        for(int s : stones){
-            countStones[s%3]++;
+        for (int s : stones) {
+            countStones[s % 3]++;
         }
-        boolean win_alice = can_win(countStones[0],countStones[1],countStones[2]) || can_win(countStones[0],countStones[2],countStones[1]);
-        return win_alice;
+        return can_win(countStones[0], countStones[1], countStones[2]) ||
+               can_win(countStones[0], countStones[2], countStones[1]);
     }
 }
