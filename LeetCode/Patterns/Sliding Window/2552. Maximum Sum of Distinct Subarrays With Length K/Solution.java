aef22540c17a71d125c1300fdc11c85829b9ae7c
Aditya B <aditya.bajoria0208@gmail.com>
2026-08-26 13:03:38 +0530

    Add Medium Java Solution for 2552. Maximum Sum of Distinct Subarrays With Length K from LeetCode

--- LeetCode/Patterns/Sliding Window/2552. Maximum Sum of Distinct Subarrays With Length K/Solution.java
+++ LeetCode/Patterns/Sliding Window/2552. Maximum Sum of Distinct Subarrays With Length K/Solution.java
@@ -1,16 +1,41 @@
 class Solution {
     public long maximumSubarraySum(int[] nums, int k) {
-        long max = 0;
+        long maxSum = 0, sum = 0;
+        int[] counts = new int[100001];
+        int duplicateCount = 0;
 
-        for (int i = 0; i < nums.length - k; i++) {
-            long a = nums[i];
-            long b = nums[i + 1];
-            long c = nums[i + 2];
-            if (a != b && b != c && c != a) {
-                long sum = a + b + c;
-                max = Math.max(max,sum);
+        for (int i = 0; i < k; i++) {
+            sum += nums[i];
+            if (counts[nums[i]] > 0) {
+                duplicateCount++;
+            }
+            counts[nums[i]]++;
+        }
+
+        if (duplicateCount == 0) {
+            maxSum = sum;
+        }
+
+        int low = 0;
+        for (int high = k; high < nums.length; high++) {
+            sum += nums[high];
+            if (counts[nums[high]] > 0) {
+                duplicateCount++;
+            }
+            counts[nums[high]]++;
+
+            int leftElement = nums[low];
+            sum -= leftElement;
+            counts[leftElement]--;
+            if (counts[leftElement] > 0) {
+                duplicateCount--;
+            }
+            low++;
+
+            if (duplicateCount == 0) {
+                maxSum = Math.max(maxSum, sum);
             }
         }
-        return max;
+        return maxSum;
     }
 }
